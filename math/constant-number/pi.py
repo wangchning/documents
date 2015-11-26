25ab0167aeaf5ac7fe79bfc207887b6741e14b03
wangchning <wangchning@163.com>
2015-11-26 20:07:06 +0800

    疏忽太久

---
+++ math/constant-number/pi.py
@@ -0,0 +1,13 @@
+import sys
+k, a, b, a1, b1 = 2, 4, 1, 12, 4
+while k<=100:
+  p, q, k = k*k, 2*k+1, k+1
+  a, b, a1, b1 = a1, b1, p*a+q*a1, p*b+q*b1
+  d = a // b
+  d1 = a1 // b1
+  while d == d1:
+    sys.stdout.write(str(d))
+    sys.stdout.flush()
+    a, a1 = 10*(a%b), 10*(a1%b1)
+    d, d1 = a//b, a1//b1
+
